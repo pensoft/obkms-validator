938541eb10bd33d92056d015a3249e0ae79b15dc
root <root@project-dev.pensoft.local>
2023-11-09 15:49:47 +0200

    Update main.php

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main(argv):
     inputUrl = ''
     opts, args = getopt.getopt(argv,"heu:n",["help","url=","errors"])
     validator = Validator()
+    withErrors = False
+    withChecks = False
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             print ('main.py --url=<url>')
@@ -18,23 +20,26 @@ def main(argv):
                 sys.stderr.write(str("The -u/--url is missing please check and try again. \n"))
             validator.setXml(arg)
         elif opt in ('-e', '--errors'):
-            validator.validate()
-            for error in validator.errors():
-                sys.stderr.write(str(error)+"\n")
-            sys.exit()
+            withErrors = True
         elif opt == '-n':
-            validator.validate()
-            sys.stdout.write(str(validator.checks())+"\n")
-            sys.exit()
-    
-    if inputUrl:
-        validator.validate()
-        hasErrors = bool(validator.errors())
-        return sys.stdout.write(str(not hasErrors)+"\n")
-
+            withChecks = True
+            
     if len(inputUrl) == 0:
         sys.exit()
-        
+    
+    validator.validate()
+    
+    if withErrors:
+        for error in validator.errors():
+            sys.stderr.write(str(error)+"\n")
+        sys.exit()
+    
+    if withChecks:
+        sys.stdout.write(str(validator.checks())+"\n")
+    
+    if inputUrl:
+        hasErrors = bool(validator.errors())
+        sys.stdout.write(str(not hasErrors)+"\n")
     
     sys.exit()
 if __name__ == "__main__":
